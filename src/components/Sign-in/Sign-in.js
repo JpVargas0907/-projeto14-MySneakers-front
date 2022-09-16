5fe80530c381d3658723f2ff62bfb0ce1aea8ed7
mateus <mateus.tigreewallet@gmail.com>
2022-09-16 01:19:33 -0300

    faltando resolver logica do axio tela 1-2-5

--- src/components/Sign-in/Sign-in.js
+++ src/components/Sign-in/Sign-in.js
@@ -1,3 +1,4 @@
+export default function SignIn() {}
 import styled from "styled-components";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
@@ -62,87 +63,67 @@ export default function SignIn() {
 const Main = styled.main`
   width: 100%;
   height: 100vh;
-
   margin: auto;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-
   div {
     display: flex;
     gap: 0.9rem;
-
     .titulo {
       color: #ffb200;
     }
     margin-bottom: 1rem;
   }
-
   h1 {
     font-weight: 700;
     font-size: 2.2rem;
     line-height: 2.9rem;
   }
-
   p {
     font-size: 1rem;
     text-align: center;
     text-decoration-line: underline;
-
     margin-top: 0.8rem;
-
     color: #ffb200;
   }
-
   form {
     width: 90%;
-
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
-
     gap: 0.8rem;
-
     input {
       width: 100%;
       height: 3rem;
-
       background: #ffffff;
       border: 0.1rem solid #c4c1c1;
       border-radius: 0.5rem;
     }
-
     input::placeholder {
       font-family: "Inter";
       font-style: normal;
       font-weight: 700;
       font-size: 1rem;
       padding-left: 0.5rem;
-
       color: #c4c1c1;
     }
-
     button {
       width: 100%;
       height: 3rem;
-
       background: #277bc0;
       border-radius: 0.5rem;
-
       border: none;
       cursor: pointer;
-
       display: flex;
       justify-content: center;
       align-items: center;
-
       font-family: "Inter";
       font-style: normal;
       font-weight: 700;
       font-size: 1.5rem;
-
       color: #ffffff;
     }
   }
