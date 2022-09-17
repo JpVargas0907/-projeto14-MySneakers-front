e8bce5cb65de13d0cff038dc334d501d4816e8d3
mateus <mateus.tigreewallet@gmail.com>
2022-09-16 23:25:49 -0300

    deixando telas mais interativas e bonitas

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import SignIn from "./components/Sign-in/Sign-in";
 import SignUp from "./components/Sign-up/Sign-up";
-import Data from "./Data/Data";
 import Context from "./contexts/UserContext";
+import Payment from "./components/Payment/Payment";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 export default function App() {
   const [token, setToken] = useState("");
@@ -18,11 +20,23 @@ export default function App() {
         setEmail,
       }}
     >
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        theme="colored"
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <BrowserRouter>
         <Routes>
           <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/data" element={<Data />} />
           <Route path="/" element={<SignIn />} />
+          <Route path="/payment" element={<Payment />} />
         </Routes>
       </BrowserRouter>
     </Context.Provider>
